ba66a0e2fc04fd81b85089b8682daca6c868b6d6
musin16 <116721500+musin16@users.noreply.github.com>
2024-01-28 20:37:40 +0100

    Add files via upload

---
+++ ChatMusinMaterial/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    LoginComponent,
+    RegistroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    IonicModule.forRoot({})
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
